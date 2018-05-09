473f44e9a3c22ac6e55fab7b60313d6d1b786f13
Cayo Hollanda <cayo.hollanda@alfaconsultoria.digital>
2018-05-09 14:56:24 -0300

    projeto + pacotes + configurações no application.properties

---
+++ src/main/java/br/com/iesp/ipc/ProjetoIpcApplication.java
@@ -0,0 +1,12 @@
+package br.com.iesp.ipc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIpcApplication.class, args);
+	}
+}
